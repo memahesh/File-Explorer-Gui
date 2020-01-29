c2b827afde9cf09638947ae8e7f530abdb8d29c8
Mahesh Medam <memahesh219@gmail.com>
2020-01-29 14:58:28 +0530

    jdksj

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -1,8 +1,34 @@
-function operate(operator) {
-	var num1 = document.querySelector('#num-1').value;
-	var num2 = document.querySelector('#num-2').value;
-	resultLambda = operator(num1, num2);
-	resultLambda(result => {
-		document.querySelector('#output').innerText = result;
+$(document).ready(function(){
+
+
+	$("form#getFilesForm").on("submit", function(e){
+		e.preventDefault();
+		let filePath = $('input[name=filePath]').val();
+		eel.getFiles(filePath)(function(ret){
+			addFilesToDOM(ret);
+		});
 	});
-}
+
+	function addFilesToDOM(ret){
+		resetContentContainer();
+		let div = $('div#contentContainer');
+		let N = ret.length;
+		for(let i=0; i<N; i++){
+			div.append(contentDOM(ret[i][0], ret[i][1]));
+		}
+	}
+
+	function contentDOM(fileName, fileType){
+		let container = $('<div>', {class:'col-md-3'});
+		let img = $('<img>', {src:'assets/'+fileType+'-icon.png', width:150});
+		let p = $('<p>', {class:'text-info text-center'}).text(fileName);
+		container.append(img);
+		container.append(p);
+		return container;
+	}
+
+	function resetContentContainer(){
+		$('div#contentContainer').html("");
+	}
+
+});
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 import eel
+import os
 
 eel.init('front-end')
 
+currPath = os.getcwd()
 
 @eel.expose
-def add(num1, num2):
-    return int(num1) + int(num2)
+def getCWD():
+	return currPath
 
+
+def fileType(fileName):
+	ext = fileName.split('.')
+
+	if len(ext) == 1:
+		return 'folder'
+
+	ext = ext[len(ext)-1]
+	if ext == 'png' or ext == 'jpg' or ext == 'gif':
+		return 'image'
+	elif ext == 'pdf':
+		return 'pdf'
+	else:
+		return 'file'
 
 @eel.expose
-def subtract(num1, num2):
-    return int(num1) - int(num2)
+def getFiles(path):
+    
+
+	filesList = os.listdir(os.path.join(currPath, path))
+
+	detailedFilesList = []
+
+	for file in filesList:
+		temp = []
+		temp.append(file)
+		temp.append(fileType(file))
+		detailedFilesList.append(temp)
+
+	return detailedFilesList
 
 
 eel.start('index.html', size=(1000, 600))
